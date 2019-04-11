edb687e39efeea84138f9c5f64417f6f98a65a38
NaagAlgates <nagharaj.cse@gmail.com>
2019-04-11 18:36:20 +1000

    Added search field without events

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-const String BTN_SEARCH_TEXT = "தேடு";
+const String HINT_SEARCH_TEXT = "தேடு";
+const String TITLE_MAIN_PAGE = APPLICATION_NAME;
 const String BTN_YES_TEXT = "ஆம்";
 const String BTN_NO_TEXT = "இல்லை";
 const String APPLICATION_NAME = "சொற்களஞ்சியம்";
-const MaterialColor APPBAR_COLOR=Colors.green;
+const String DISPLAY_MESSAGE_NO_WORDS="தயவுசெய்து ஒரு வார்த்தையை உள்ளிடவும்";
+const MaterialColor COLOR_APPBAR=Colors.green;
+const Color COLOR_DISABLED=Colors.grey;
+const Color COLOR_ENABLED=COLOR_APPBAR;
+const Color COLOR_WHITE = Colors.white;
+const Color COLOR_BLACK = Colors.black;
+const double FONT_SIZE_REGULAR_20=20.0;
+const double PADDING_REGULAR_10 = 10.0;
--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,72 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      theme: ThemeData(
+        textSelectionColor: COLOR_APPBAR,
+        backgroundColor:COLOR_WHITE,
+        cursorColor: COLOR_APPBAR
+      ),
+      title: TITLE_MAIN_PAGE,
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text(APPLICATION_NAME),
-          backgroundColor: APPBAR_COLOR,
+          backgroundColor: COLOR_APPBAR,
         ),
         body: new Center(
-          child: new Text(APPLICATION_NAME),
+          child: new Column(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: TextFormField(
+                      autofocus: false,
+                      autocorrect: false,
+                      autovalidate: false,
+                      obscureText: false,
+                      textDirection: TextDirection.ltr,
+                      textInputAction: TextInputAction.search,
+                      maxLength: 20,
+                      maxLines: 1,
+                      controller: _textController,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: FONT_SIZE_REGULAR_20,
+                        color: COLOR_BLACK,
+                        decorationColor: COLOR_APPBAR
+                      ),
+                      validator: (val) => val.length < 0 ? DISPLAY_MESSAGE_NO_WORDS : null,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(PADDING_REGULAR_10),
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(0.0),
+                            child: Icon(
+                              Icons.search,
+                              color: COLOR_APPBAR,
+                            ), // icon is 48px widget.
+                          ),
+                          suffixIcon: Padding(
+                            padding: EdgeInsets.all(0.0),
+                            child: IconButton(
+                              icon: IconButton(
+                                  icon: Icon(Icons.clear),
+                                  disabledColor: COLOR_DISABLED,
+                                  highlightColor: COLOR_ENABLED, onPressed: () {_textController.clear();},
+                                  ),
+                              onPressed: () {_textController.clear();},
+                            ), // icon is 48px widget.
+                          ),
+                          hintText: HINT_SEARCH_TEXT,
+                          hintStyle: TextStyle(fontSize: FONT_SIZE_REGULAR_20)),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
