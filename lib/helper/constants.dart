a04deac5a2ba534e861f28ea51780a40d123fc97
NaagAlgates <nagharaj.cse@gmail.com>
2019-04-11 20:49:30 +1000

    UI included and tested in IOS

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
+//Hints
 const String HINT_SEARCH_TEXT = "தேடு";
+
+//Application Constants
+const String APPLICATION_NAME = "சொற்களஞ்சியம்";
 const String TITLE_MAIN_PAGE = APPLICATION_NAME;
+const String DATE_FORMAT="MM-DD-YYYY";
+
+//Action items label
 const String BTN_YES_TEXT = "ஆம்";
 const String BTN_NO_TEXT = "இல்லை";
-const String APPLICATION_NAME = "சொற்களஞ்சியம்";
+
+//Display Message text
 const String DISPLAY_MESSAGE_NO_WORDS="தயவுசெய்து ஒரு வார்த்தையை உள்ளிடவும்";
+const String DISPLAY_2ND_COLUMN_HEADING = "தினம் ஒரு சொல்!";
+const String DISPLAY_3RD_COLUMN = "தினம் ஒரு குறள்!";
+
+//Constant Colors
 const MaterialColor COLOR_APPBAR=Colors.green;
 const Color COLOR_DISABLED=Colors.grey;
 const Color COLOR_ENABLED=COLOR_APPBAR;
 const Color COLOR_WHITE = Colors.white;
 const Color COLOR_BLACK = Colors.black;
+
+//Ints
+const double FONT_SIZE_REGULAR_15=15.0;
 const double FONT_SIZE_REGULAR_20=20.0;
+const double FONT_SIZE_HEADING_30=30.0;
+
+//Padding
 const double PADDING_REGULAR_10 = 10.0;
+const double PADDING_REGULAR_15 = 15.0;
